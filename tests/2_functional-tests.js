d139bd46646dfd39a11bd529154f154f41a0196e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-02-03 10:28:29 +0100

    Create test placeholders

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,12 +1,35 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
 const assert = chai.assert;
-const server = require('../server.js');
+const server = require("../server.js");
 
 chai.use(chaiHttp);
 
-let Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
+const translator = new Translator();
 
-suite('Functional Tests', () => {
+suite("Functional Tests", () => {
+  test("Translation with text and locale fields: POST request to `/api/translate`", () => {
+    assert.fail();
+  });
 
+  test("Translation with text and invalid locale field: POST request to `/api/translate`", () => {
+    assert.fail();
+  });
+
+  test("Translation with missing text field: POST request to `/api/translate`", () => {
+    assert.fail();
+  });
+
+  test("Translation with missing locale field: POST request to `/api/translate`", () => {
+    assert.fail();
+  });
+
+  test("Translation with empty text: POST request to `/api/translate`", () => {
+    assert.fail();
+  });
+
+  test("Translation with text that needs no translation: POST request to `/api/translate`", () => {
+    assert.fail();
+  });
 });
