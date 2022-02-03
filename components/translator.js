9e1da7a353f35a8f192dc0600ed3a3bc535ff60c
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-02-03 11:43:29 +0100

    Implement translator error handling

--- components/translator.js
+++ components/translator.js
@@ -1,10 +1,53 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const americanOnly = require("./american-only.js");
+const americanToBritishSpelling = require("./american-to-british-spelling.js");
+const americanToBritishTitles = require("./american-to-british-titles.js");
+const britishOnly = require("./british-only.js");
 
 class Translator {
+  translate = (text, locale, highlightTranslatedWords) => {
+    /**
+     * INPUT SANITISING / ERROR HANDLING
+     */
+    if (!this._validateLocale(locale)) {
+      return {
+        error: "Invalid value for locale field",
+      };
+    }
 
+    /**
+     * TRANSLATION
+     */
+
+    // First character of the input is ALWAYS shifted to uppercase
+    // other than that, case is ignored (including for example first char of the second sentence)
+
+    // words get translated no matter what case they're in
+    // the output translation is always the default case
+    // UNLESS it's the first character of the entire string, then the
+    // first letter gets capitalized. so probably do that last.
+
+    // translated words get wrapped in `<span class=\"highlight\">WORD</span>
+    const translation = "todo";
+
+    if (translation === text) {
+      return {
+        text: text,
+        translation: "Everything looks good to me!",
+      }
+    }
+
+    return {
+      text: text,
+      translation: translation,
+    }
+  };
+
+  _validateLocale = (locale) => {
+    if (locale !== "american-to-british" || "british-to-american") {
+      return false;
+    }
+    return true;
+  };
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,28 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const { request } = require("chai");
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  /**
+   * expected request body (json):
+   * locale: string - either 'american-to-british' or 'british-to-american'
+   * text: stirng - anything, not empty
+   */
+  app.route("/api/translate").post((req, res) => { 
+    // make sure the body string is not empty
+    if (req.body.text === "") {
+      return res.json({ error: "No text to translate" });
+    }
+
+    // make sure no required body parameters are missiong
+    if (!req.body.text || !req.body.locale) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+
+    const response = translator.translate(req.body.text, req.body.locale, true);
+    return res.json(response);
+  });
 };
